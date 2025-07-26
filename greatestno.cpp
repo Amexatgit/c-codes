e3f7a4b628b60d9fd3dd412fb28f7414c49b8419
Amexatgit <professionalamex@gmail.com>
2025-07-26 10:28:45 +0000

    DSA sigma Assingment

---
+++ greatestno.cpp
@@ -0,0 +1,21 @@
+ #include<iostream>
+
+ using namespace std;
+
+ int main() {
+   int a,b;
+
+   cout<<"enter First value";
+   cin>>a;
+   cout<<"enter Second value";
+   cin>>b;
+
+   if(a>b){
+    cout<<a;
+   }
+   else{
+    cout<<b;
+   }
+
+    return 0;
+ }
